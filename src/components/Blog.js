6b8b4668f56c8399a31c4d3d2422b1718af086fc
cdrbbt <egor.bykadorov@gmail.com>
2021-02-12 18:38:41 +0200

    5.7 blog details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+//Toggleable seems to actually work just fine?
+//import Toggleable from './Toggleable'
+
+const Blog = ({ blog }) => {
+
+  const [visibility, setVisibility] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const label = visibility ? 'hide' : 'details'
+
+  const changeVisibility = () => setVisibility(!visibility)
+
+  const details = () => {
+    if (!visibility) return null
+    return (
+      <div>
+        <p>{`author: ${blog.author}`}</p>
+        <p>{`likes: ${blog.likes}`}</p>
+        <p>{`url: ${blog.url}`}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={changeVisibility}>{label}</button>
+      {details()}
+    </div>
+  )}
 
 export default Blog
